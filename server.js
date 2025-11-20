2c01427890f9ba68775e6a4aa3533f666449cdcd
Beso <94741201+Bes23@users.noreply.github.com>
2025-11-20 18:39:51 +0400

    feat: add handlers folder, hadnleGet function and integrate into main server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import http from "node:http";
 import { serveStatic } from "./utils/serverStatic.js";
 
-
 const PORT = 8000;
 const __dirname = import.meta.dirname;
 
 const server = http.createServer(async (req, res) => {
-  await serveStatic(req, res, __dirname);
+  if(req.url === '/api') {
+    if(req.method === 'GET') {
+      return await handleGet(res)
+    }
+  }
+  if (!req.url.startsWith("/api")) {
+    return await serveStatic(req, res, __dirname);
+  }
 });
 
 server.listen(PORT, () =>
