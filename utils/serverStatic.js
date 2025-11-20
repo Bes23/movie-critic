1c4b48bd3452acaf67c7a67bff01cdebd8ca9d34
Beso <94741201+Bes23@users.noreply.github.com>
2025-11-20 14:47:51 +0400

    refactor: integrate sendResponse utility into servestatic handler

--- utils/serverStatic.js
+++ utils/serverStatic.js
@@ -1,6 +1,13 @@
 import path from "node:path";
+import fs from "node:fs/promises";
+import { sendResponse } from "./sendResponse.js";
 
-export function serveStatic() {
+export async function serveStatic(req, res, baseDir) {
   const filePath = path.join(baseDir, "public", "index.html");
-  console.log(filePath);
+  try {
+    const content = await fs.readFile(filePath);
+    sendResponse(res, 200, "text/html", content);
+  } catch (error) {
+    console.log(error);
+  }
 }
