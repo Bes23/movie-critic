502743c7f17b23f455eb0fe0393809879c5bf402
Beso <94741201+Bes23@users.noreply.github.com>
2025-11-20 22:10:35 +0400

    feat: add frontend form submission to POST reviews

--- public/index.js
+++ public/index.js
@@ -48,33 +48,4 @@ if (container) {
       : "Read full review";
   });
 }
-// const cards = document.querySelectorAll(".review-card");
 
-// cards.forEach((card) => {
-//   const btn = card.querySelector(".toggle-btn");
-//   const text = card.querySelector(".review-text");
-
-//   btn.addEventListener("click", () => {
-//     text.classList.toggle("expanded");
-
-//     btn.textContent = text.classList.contains("expanded")
-//       ? "Show less"
-//       : "Read full review";
-//   });
-// });
-
-// auto-generate date
-
-const dateInput = document.querySelector("#review-date");
-
-const now = new Date();
-const formattedDate = now.toLocaleString("en-US", {
-  day: "2-digit",
-  month: "short",
-  year: "numeric",
-  hour: "2-digit",
-  minute: "2-digit",
-  hour12: false,
-});
-
-dateInput.value = formattedDate;
